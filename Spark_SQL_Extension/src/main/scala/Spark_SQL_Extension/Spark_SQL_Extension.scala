15fd1ca2d65b695be800d0cc146cec8f7f02a07f
Naresh-Agra <Naresh-Agra@users.noreply.github.com>
2019-12-30 13:41:38 +0530

    Edited

--- Spark_SQL_Extension/src/main/scala/Spark_SQL_Extension/Spark_SQL_Extension.scala
+++ Spark_SQL_Extension/src/main/scala/Spark_SQL_Extension/Spark_SQL_Extension.scala
@@ -13,6 +13,7 @@ import org.apache.spark.sql.jdbc._
 import scala.io.Source
 import org.apache.spark.sql.functions.explode
 import org.apache.spark.sql.functions._
+import com.mysql.cj.jdbc.Driver
 
 object Spark_SQL_Extension {
   
@@ -29,8 +30,26 @@ object Spark_SQL_Extension {
               
     import spark.implicits._
     
+    val html=Source.fromURL("https://randomuser.me/api/0.8/?results=10") // Url buffered source
+    val urlstring=html.mkString //making url as string
+    val rddstring=sc.parallelize(List(urlstring)) //converting string to rdd
+    val web_data=spark.read.json(rddstring) //converting rdd to dataframe as json
+    web_data.show()
+    web_data.printSchema()
+
+    val processed_data1=web_data.withColumn("naresh", explode($"results")).selectExpr("naresh.user.name.first as Name","naresh.user.location.city as City","naresh.user.username as UserName","naresh.user.picture.thumbnail as Thumnail")
+    println
+    println("Reading data as DSL Operations")
+    processed_data1.show(false)
+    println
+    println("Reading as SQL Operations")
+    processed_data1.createOrReplaceTempView("processed")
+    spark.sql("select * from processed").show(false)
     
-    
+    println
+    println("Writing table content to local")
+    processed_data1.write.format("csv").mode("append").save("file:///E://Workouts//Executed_Results//sql_ext1")
+    println("Saved")
     
     
   }
